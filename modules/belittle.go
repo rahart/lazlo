73a3eb122760e8751ce1953be7c5f9876b89f0bf
Mike Dillon <mike.dillon@entangledmedia.net>
2015-08-05 14:41:48 -0600

    Started a new module to belittle people.
    also added swp for gitignore

---
+++ modules/belittle.go
@@ -0,0 +1,37 @@
+package modules
+
+import (
+	lazlo "github.com/djosephsen/lazlo/lib"
+	"math/rand"
+	"time"
+)
+
+var Belittle = &lazlo.Module{
+    Name: `Belittle`,
+    Usage: `%BOTNAME belittle <username>`,
+    Run: belittleUser,
+}
+
+func belittleUser(broker *lazlo.Broker) {
+    cb := b.MessageCallback(`(?i)(belittle) (@*[\w-]+)`, true)
+    for {
+        select {
+        case newReq := <-cb.Chan:
+            go makeFunOfUser(b, newReq)
+        }
+    }
+}
+
+func makeFunOfUser(b *lazlo.Broker, req lazlo.PatternMatch) {
+    lazlo.logger.Info("Making fun of a user: %s", req[2])
+}
+
+func randomShitSaying() string {
+    now := time.Now()
+    rand.Seed(int64(now.Unix())
+    replies := []String{
+        " you suck." 
+    }
+
+    return replies[rand.Intn(len(replies)-1)]
+}
